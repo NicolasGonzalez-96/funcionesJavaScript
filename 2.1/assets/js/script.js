126dcd68ec6f80aeed07a3feba1719c6b396a825
Nicolás González <nicogonzas@gmail.com>
2023-11-03 03:03:44 -0300

    agregado punto 2.3 ejercicio

--- 2.1/assets/js/script.js
+++ 2.1/assets/js/script.js
@@ -1,5 +1,9 @@
+function pintar(color = 'green') {
+  ele.style.backgroundColor = color;
+}
+
 const ele = document.getElementById("ele1");
 
 ele.addEventListener("click", function() {
-  ele.style.backgroundColor = 'yellow';
+  pintar('yellow');
 });
